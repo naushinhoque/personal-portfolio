48098e6e58bf629db60a13a70d098c1052aaa01a
naushinhoque <fnmh@yahoo.com>
2023-10-09 14:14:32 -0500

    navbar started

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+
+export const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('home');
+    return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+            <img src={''} alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggle-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+        </Nav>
+        <span className="navbar-text">
+            <div className='social-icon'>
+                <a href="#"><img src={} alt="" /></a>
+                <a href="#"><img src={} alt="" /></a>
+                <a href="#"><img src={} alt="" /></a>
+            </div>
+            <button className='vvd' onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+        </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );   
+
+}
